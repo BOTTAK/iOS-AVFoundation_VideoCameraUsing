7a8f8c9f673f48abde8daf807125b7237cf7b9ce
Пенькевич Василий <djbottak@gmail.com>
2019-11-07 10:00:48 +0300

    Initial Commit

---
+++ CameraTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameraTest
+//
+//  Created by Пенькевич Василий on 11/7/19.
+//  Copyright © 2019 Пенькевич Василий. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
